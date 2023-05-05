30d1c27fc38efaa0de87c80560d5a8a050d5e06b
julio2505 <juliomeza2510@outlook.com>
2023-05-05 12:35:41 -0600

    Added font

---
+++ Assets/Scripts/SceneManager2.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager2 : MonoBehaviour
+{
+    public static void cambiaEscena(string escena)
+    {
+        SceneManager.LoadScene("Login_2");
+    }
+
+    public static void cambiaEscena2(string escena)
+    {
+        SceneManager.LoadScene("Login_3");
+    }
+
+    public static void cambiaEscena3(string escena)
+    {
+        SceneManager.LoadScene("MarioGame");
+    }
+
+    public static void salir()
+    {
+        Application.Quit();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
+}
