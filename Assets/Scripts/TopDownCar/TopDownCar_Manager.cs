7a38826e7d3899144844664ba8668ba4795a6e5d
LGnan <leogarcia2003@hotmail.com>
2023-05-20 20:17:05 -0600

    Avance pero con error de consola
    Voy en la parte del render de props

--- Assets/Scripts/TopDownCar/TopDownCar_Manager.cs
+++ Assets/Scripts/TopDownCar/TopDownCar_Manager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 using TMPro;
 
@@ -17,6 +18,23 @@ public class TopDownCar_Manager : MonoBehaviour
     private GameManager gamemanager;
     private Pregunta pregunta;
 
+    [SerializeField]
+    GameObject[] triggers;
+
+    [SerializeField]
+    GameObject[] finishLine;
+
+    [SerializeField]
+    GameObject[] obstacle;
+
+    List<Vector3> positions = new List<Vector3>
+    {
+        new Vector3(-5.4f, 6f, 0),
+        new Vector3(-1.8f, 6f, 0),
+        new Vector3(1.8f, 6f, 0),
+        new Vector3(5.4f, 6f, 0),
+    };
+
     void Start()
     {
         gamemanager = FindObjectOfType<GameManager>();
@@ -26,6 +44,12 @@ public class TopDownCar_Manager : MonoBehaviour
         {
             Respuesta res = pregunta.respuestas[i];
             respuestasTexto[i].text = res.textoRespuesta;
+            Trigger_Option to = triggers[i].GetComponent<Trigger_Option>();
+            to.isCorrect = res.idRespuesta; // revisar
+            if (res.isCorrect == 1)
+            {
+                to.isCorrect = true;
+            }
         }
     }
 
--- Assets/Scripts/TopDownCar/Trigger_Option.cs
+++ Assets/Scripts/TopDownCar/Trigger_Option.cs
@@ -4,9 +4,31 @@ using UnityEngine;
 
 public class Trigger_Option : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("Opción");
-        
+    public int idRespuesta = 0;
+    private GameManager gamemanager;
+    public bool isCorrect = false;
+
+    void Start()
+    {
+        gamemanager = FindObjectOfType<GameManager>();
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Opción");
+        StartCoroutine(FinishGame(isCorrect, idRespuesta));
+    }
+
+    IEnumerator FinishGame(bool win, int idRespuesta)
+    {
+        yield return new WaitForSeconds(3);
+        if (win)
+        {
+            gamemanager.OnCorrectAnswer(idRespuesta);
+        }
+        else
+        {
+            gamemanager.OnWrongAnswer(idRespuesta);
+        }
+    }
 }
