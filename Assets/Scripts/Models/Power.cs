83316f2efeeeefbbe5220836a304c6339ee33159
julio2505 <juliomeza2510@outlook.com>
2023-06-12 17:01:06 -0600

    Added API connection for powers

---
+++ Assets/Scripts/Models/Power.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[System.Serializable]
+public class Power
+{
+    public int idAlumno;
+    public int idPoder;
+    public int cantidad;
+}
